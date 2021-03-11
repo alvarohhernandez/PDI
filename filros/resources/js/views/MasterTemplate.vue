e175a5640d02f24114f59983737a385609ffd1d8
Alvaro <a_90_hernandez@hotmail.com>
2021-03-11 11:24:09 -0600

    Red and Blue Filter

--- filros/resources/js/views/MasterTemplate.vue
+++ filros/resources/js/views/MasterTemplate.vue
@@ -13,8 +13,8 @@
     
           <md-menu-content class="maxHeightNone">
             <md-menu-item @click="grises1">Tono de Grises 1</md-menu-item>
-            <md-menu-item>Tono de Grises 2</md-menu-item>
-            <md-menu-item>Tono de Grises 3</md-menu-item>
+            <md-menu-item @click="blue">Blue</md-menu-item>
+            <md-menu-item @click="red">Red</md-menu-item>
             <md-menu-item>My Item 3</md-menu-item>
             <md-menu-item>My Item 3</md-menu-item>
             <md-menu-item>My Item 3</md-menu-item>
@@ -24,8 +24,14 @@
         </md-menu>
         </div>
 
-        <img :src="imageUrl" id="original-image"/>
-        <canvas id="canvas"></canvas>
+        <div class="md-layout md-gutter md-alignment-center-center">
+          <div class="md-layout-item md-size-30">
+            <img :src="imageUrl" id="original-image"/>
+          </div>
+          <div class="md-layout-item md-size-30">
+            <canvas id="canvas"></canvas>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -34,8 +40,11 @@
 <script>
   export default {
     data: () => ({
-      image: null,
       imageUrl: null,
+      canvas: null,
+      ctx: null,
+      width: 0,
+      height: 0,
     }),
     methods: {
       uploadImage() {
@@ -45,39 +54,69 @@
         const files = event.target.files
         let filename = files[0].name
         const fileReader = new FileReader()
-        fileReader.addEventListener('load', () => {
-          this.imageUrl = fileReader.result
+        fileReader.addEventListener('load', async () => {
+          this.imageUrl = await fileReader.result
         })
         fileReader.readAsDataURL(files[0])
-        this.image = files[0]
       },
-      grises1() {
-        const img = new Image();
-        img.src = this.imageUrl;
-        const width = img.width;
-        const height = img.height;
-        const canvas = document.getElementById('canvas');
-        canvas.width = width;
-        canvas.height = height;
-        const ctx = canvas.getContext('2d');
-        console.log('Width', img.width);
-        img.onload = () => {
-          ctx.drawImage(img, 0, 0);
-          img.style.display = 'none';
-          const imageData = ctx.getImageData(0, 0, width, height);
-          for (var i = 0; i < imageData.data.length; i += 4) {
-            const gray = Math.trunc((imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2])/3);
-            imageData.data[i] = gray;
-            imageData.data[i + 1] = gray;
-            imageData.data[i + 2] = gray;
+      async grises1() {
+          await this.loadContext();
+          for (var i = 0; i < this.canvas.width; i++) {
+            for (let j = 0; j < this.canvas.height; j++) {
+              const imageData = this.ctx.getImageData(i, j, 1, 1);
+              const gray = Math.trunc((imageData.data[0] + imageData.data[1] + imageData.data[2])/3);
+              imageData.data[0] = gray;
+              imageData.data[1] = gray;
+              imageData.data[2] = gray;
+              this.ctx.putImageData(imageData, i, j);
+            }
           }
-          ctx.putImageData(imageData, 0, 0);
-        };
+      },
+      async blue() {
+          await this.loadContext();
+          for (var i = 0; i < this.canvas.width; i++) {
+            for (let j = 0; j < this.canvas.height; j++) {
+              const imageData = this.ctx.getImageData(i, j, 1, 1);
+              imageData.data[0] = 0;
+              imageData.data[1] = 0;
+              this.ctx.putImageData(imageData, i, j);
+            }
+          }
+      },
+      async red() {
+          await this.loadContext();
+          for (var i = 0; i < this.canvas.width; i++) {
+            for (let j = 0; j < this.canvas.height; j++) {
+              const imageData = this.ctx.getImageData(i, j, 1, 1);
+              imageData.data[1] = 0;
+              imageData.data[2] = 0;
+              this.ctx.putImageData(imageData, i, j);
+            }
+          }
+      },
+      loadContext() {
+        return new Promise((resolve) => {
+          let width = 0;
+          let height = 0;
+          const img = new Image();
+          img.src = this.imageUrl;
+          img.onload = () => {
+            this.canvas = document.getElementById('canvas');
+            this.canvas.width = img.width;
+            this.canvas.height = img.height;
+            this.ctx = this.canvas.getContext('2d');
+            this.ctx.drawImage(img, 0, 0);
+            img.style.display = 'none';
+            resolve();
+          }
+        });
       },
     },
     watch: {
-      image(newValue) {
-        console.log('New Value', newValue);
+      imageUrl(newValue) {
+        if (newValue) {
+          this.loadContext();
+        }
       }
     }
   }
